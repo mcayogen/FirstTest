6dc8a402c6c0dab3c8e9d318ff4705ac5cd868df
mcayogen <mca.yogen@gmail.com>
2021-02-14 10:32:45 +0530

    Test msg

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace WebApplication1
     {
         public static void Main(string[] args)
         {
+            //Test Message
             CreateWebHostBuilder(args).Build().Run();
         }
 
